20138b92b7e7356b1721f6dc7210efcec3b249ac
Nick Dolley <ndolley@inviqa.com>
2016-08-17 11:55:15 +0100

    initial commit

---
+++ web/sites/default/settings.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @file
+ * Settings file.
+ */
+
+/**
+ * Salt for one-time login links, cancel links, form tokens, etc.
+ */
+$settings['hash_salt'] = 'l54drEw2Ty44FshvkQ8IBr9tVIOWMbNy24XiAYzdp43TpiO8FjnaIWrYNFGCIBFq3DaJZWwWOA';
+
+/**
+ * Access control for update.php script.
+ */
+$settings['update_free_access'] = FALSE;
+
+/**
+ * Load services definition file.
+ */
+$settings['container_yamls'][] = __DIR__ . '/services.yml';
+
+$settings['install_profile'] = 'standard';
+$config_directories['sync'] = 'sites/default/files/config_X69137mLRxy_7czl0DQFBvmh3Exs1wb5zmexw_l0tkJU5VCDU_u3cHEDtT3a8J-u2b_x4-Pc2Q/sync';
+
+/**
+ * Load local development override configuration, if available.
+ *
+ * Keep this code block at the end of this file to take full effect.
+ */
+if (file_exists(__DIR__ . '/settings.local.php')) {
+  include __DIR__ . '/settings.local.php';
+}
